24f66f7f430ef8fd72f1d746bc85bf2dd80caef5
Michael Trommer <mictro@gmail.com>
2023-09-26 23:11:32 +0200

    create or edit

--- plugins/blog/slice/action.rb
+++ plugins/blog/slice/action.rb
@@ -1,5 +1,26 @@
 module Ha2itat::Slices::Blog
   class Action < Hanami::Action
     instance_eval(&Ha2itat::CD(:action))
+
+    def create_or_edit_post(req, res)
+      post = adapter.with_user(session_user(req)).by_slug(req.params[:slug])
+      if req.post?
+        params = req.params.to_hash
+
+        pimg = params.delete(:image)
+        post = adapter.update_or_create(params)
+
+        if pimg
+          adapter.upload(post, pimg[:tempfile])
+        end
+        adapter.with_user(session_user(req)).store(post)
+        return post
+      end
+      post
+    end
+
+    def adapter
+      Ha2itat.adapter(:blog)
+    end
   end
 end
