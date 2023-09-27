267e29265e23d3d8e1a02d7578f5a0c489093793
Michael Trommer <mictro@gmail.com>
2023-09-27 16:28:19 +0200

    no debug output

--- plugins/blog/slice/actions/togglepublish.rb
+++ plugins/blog/slice/actions/togglepublish.rb
@@ -7,7 +7,6 @@ module Ha2itat::Slices
           post = by_slug(req)
           return unless post
           if post.draft?
-            p :draft
             adapter.to_post(post)
 
             # if Habitat.quart.plugins.enabled?(:vgwort)
@@ -18,7 +17,6 @@ module Ha2itat::Slices
             #   end
             # end
           else
-            p :published
             adapter.to_draft(post)
           end
           res.redirect_to(redirect_target_from_request(req) || path(:backend_blog_show, slug: post.slug))
