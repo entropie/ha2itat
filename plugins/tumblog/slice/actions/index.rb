b3bd28347b3b0ae5410130ae7ce7a03ac36b4701
Michael Trommer <mictro@gmail.com>
2023-10-04 00:46:53 +0200

    redirect to last page when pager is empty

--- plugins/tumblog/slice/actions/index.rb
+++ plugins/tumblog/slice/actions/index.rb
@@ -8,6 +8,11 @@ module Ha2itat::Slices
         def handle(req, res)
           posts = adapter.with_user(session_user(req)).entries.sort_by{|p| p.created_at }.reverse
           pager = Pager.new(req.params.to_hash, posts, 2)
+
+          if pager.current_items.size == 0 and pager.current_page > 1
+            res.redirect_to(path(:backend_tumblog_index, page: "last"))
+          end
+          
           pager.link_proc = -> (n) { routes.path(:backend_tumblog_index, page: n) }
           res.render(view, pager: pager)
         end
