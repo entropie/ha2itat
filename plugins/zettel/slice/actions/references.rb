ca79e70061a2cc797f7909eae8fa9f19251842dd
Michael Trommer <mictro@gmail.com>
2023-10-05 21:34:31 +0200

    params[ref] mandatory

--- plugins/zettel/slice/actions/references.rb
+++ plugins/zettel/slice/actions/references.rb
@@ -5,11 +5,17 @@ module Ha2itat::Slices
 
         include Ha2itat.h(:pager)
 
+        params do
+          required(:ref).filled("string")
+        end
+
         def handle(req, res)
-          sheets = adapter.with_user(session_user(req)).by_reference_sorted( req.params[:ref] )
-          # pager = Pager.new(req.params.to_hash, sheets)
-          # pager.link_proc = -> (n) { routes.path(:backend_zettel_references, slug: req.params[:slug], page: n) }
-          res.render(view, sheets: sheets)
+          if req.params.valid?
+            sheets = adapter.with_user(session_user(req)).by_reference_sorted( req.params[:ref] )
+            # pager = Pager.new(req.params.to_hash, sheets)
+            # pager.link_proc = -> (n) { routes.path(:backend_zettel_references, slug: req.params[:slug], page: n) }
+            res.render(view, sheets: sheets)
+          end
         end
       end
     end
