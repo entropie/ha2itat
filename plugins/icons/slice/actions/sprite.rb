8043e73b2e538b8b057ffe22ce0a7fed8a282f47
Michael Trommer <mictro@gmail.com>
2023-10-08 17:12:41 +0200

    test

--- plugins/icons/slice/actions/sprite.rb
+++ plugins/icons/slice/actions/sprite.rb
@@ -6,6 +6,7 @@ module Ha2itat::Slices
         def handle(req, res)
           complete_file = Ha2itat.root.join("plugins/icons/assets/feather-sprite.svg")
           res.format = "image/svg+xml"
+          res.cache_control :public, max_age: 60*60*24*32
           res.body = ::File.read(complete_file)
         end
       end
