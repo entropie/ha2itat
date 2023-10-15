2b2d8e002412dd8f7409b966426c3c75707134c2
Michael Trommer <mictro@gmail.com>
2023-10-15 20:23:58 +0200

    use default pager limit

--- plugins/tumblog/slice/actions/index.rb
+++ plugins/tumblog/slice/actions/index.rb
@@ -7,7 +7,7 @@ module Ha2itat::Slices
 
         def handle(req, res)
           posts = adapter.with_user(session_user(req)).entries.sort_by{|p| p.created_at }.reverse
-          pager = Pager.new(req.params.to_hash, posts, 2)
+          pager = Pager.new(req.params.to_hash, posts)
 
           if pager.current_items.size == 0 and pager.current_page > 1
             res.redirect_to(path(:backend_tumblog_index, page: "last"))
