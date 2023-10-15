c2915f6b27553a24502f0f5faf664cdaecb10e2e
Michael Trommer <mictro@gmail.com>
2023-10-15 19:45:15 +0200

    app cleaned

--- assets/skel/config/app.rb
+++ assets/skel/config/app.rb
@@ -5,12 +5,12 @@ require "ha2itat"
 
 require_relative "h2-settings"
 
+SESSION_KEY = '%%identifier%%.session'
+SESSION_EXPIRY_TIME_IN_SECONDS = 60*60*24*365
+
 module %%Identifier%%
 
   class App < Hanami::App
-    SESSION_KEY = '%%identifier%%.session'
-
-    SESSION_EXPIRY_TIME_IN_SECONDS = 60*60*24*365
 
     config.logger.filters = config.logger.filters + ["token", "password1"]
 
@@ -29,39 +29,30 @@ module %%Identifier%%
       config.logger.options[:colorize] = true
     end
 
-    Ha2itat::I18n.init
-
     instance_eval(&Ha2itat::CD(:slice))
-
-    Ha2itat.quart.plugins do |plugs|
-      Ha2itat.log "loading plugins"
-
-      plugs.register(:backend)
-      plugs.register(:user)
-      plugs.register(:snippets)
-      plugs.register(:galleries)
-      plugs.register(:icons)
-
-      # plugs.register(:blog)
-      # plugs.register(:tumblog)
-      # plugs.register(:booking)
-      # plugs.register(:zettel)
-      # plugs.register(:notifier)
-      # plugs.register(:bagpipe)
-
-      plugs.write_javascript_include_file!
-    end
-
-    config.actions.sessions = :cookie, {
-      key: SESSION_KEY,
-      secret: SECRET,
-      expire_after: SESSION_EXPIRY_TIME_IN_SECONDS
-    }
 
   end
 end
 
 
+Ha2itat.quart.plugins do |plugs|
+  Ha2itat.log "loading plugins"
 
+  plugs.register(:backend)
+  plugs.register(:user)
+  plugs.register(:snippets)
+  plugs.register(:galleries)
+  plugs.register(:icons)
+
+  # plugs.register(:blog)
+  # plugs.register(:tumblog)
+  # plugs.register(:booking)
+  # plugs.register(:zettel)
+  # plugs.register(:notifier)
+  # plugs.register(:bagpipe)
+
+  plugs.write_javascript_include_file!
+end
+Ha2itat::I18n.init
 
 require_relative "h2-ext"
