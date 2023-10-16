3427799dfbf7a5c3f0c483bff8bb58614073b417
Michael Trommer <mictro@gmail.com>
2023-10-16 20:31:13 +0200

    bookmarklet tool

---
+++ lib/ha2itat/cli/tools.rb
@@ -0,0 +1,32 @@
+module Ha2itat
+  module CLI
+    module Tools
+      class Bookmarklet < Dry::CLI::Command
+        desc "generate bookmarklate"
+
+        argument :name, type: :string,  required: true, desc: "username"
+        argument :route, type: :string,  required: true, desc: "internal route"
+        argument :goto, type: :string,  required: false, desc: "goto after"
+
+        def bookmarklet(goto = nil)
+          gotostr = ""
+          if goto
+            gotostr = "&goto=#{goto}"
+          end
+          %Q|javascript:(function(){var url=encodeURI(document.location.href),endp="%s",token="%s";document.location.href=endp+"?token="+token+"#{gotostr}&content="+url;}());|
+        end
+
+        def call(name:, route:, goto:, **options)
+          require_relative Dir.pwd + "/config/app"
+          user = Ha2itat.adapter(:user).by_name(name)
+          route = Hanami.app.boot["routes"].path(route.to_sym)
+
+          raise "no user named `#{name}' found for `#{Ha2itat.quart.identifier}'" unless user
+          raise "no route named`#{name}' found for `#{Ha2itat.quart.identifier}'" unless route
+          puts
+          puts bookmarklet(goto) % [::File.join(Ha2itat.C(:host), route), user.token]
+        end
+      end
+    end
+  end
+end
