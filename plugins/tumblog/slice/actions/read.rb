0bee728c16711b1108a7aca1b16bf7414892efda
Michael Trommer <mictro@gmail.com>
2023-10-18 21:07:54 +0200

    temp

--- plugins/tumblog/slice/actions/read.rb
+++ plugins/tumblog/slice/actions/read.rb
@@ -1,11 +1,20 @@
 module Ha2itat::Slices
   module Tumblog
     module Actions
-      class Read < Action
+
+      class Read < Hanami::Action
+        include ActionMethodsCommon
 
         def handle(req, res)
-          path = params_path(req.params)
-          adptr = adapter.with_user(session_user(req))
+          path = params_path(req.params).shift
+          userid, postid, * = path.split("/")
+          adptr = adapter(:tumblog).with_user(session_user(req))
+          # unless adptr.user
+          #   a = adapter(:tumblog).with_user( adapter(:user).by_id(userid))
+          #   p a.by_id(postid)
+
+          # end
+
           res.body = ::File.read(::File.join(adptr.datadir, path))
         end
       end
