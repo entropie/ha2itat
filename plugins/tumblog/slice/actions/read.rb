058697cb0a24dd1f479eb2a5945f8d9abd9dacb5
Michael Trommer <mictro@gmail.com>
2023-10-20 04:54:05 +0200

    format

--- plugins/tumblog/slice/actions/read.rb
+++ plugins/tumblog/slice/actions/read.rb
@@ -9,13 +9,16 @@ module Ha2itat::Slices
           path = params_path(req.params).shift
           userid, postid, * = path.split("/")
           adptr = adapter(:tumblog).with_user(session_user(req))
+
+          target_filename = ::File.join(adptr.datadir, path)
+
+          format = target_filename.split(".").last.to_sym
+          res.format = format
           # unless adptr.user
           #   a = adapter(:tumblog).with_user( adapter(:user).by_id(userid))
           #   p a.by_id(postid)
-
           # end
-
-          res.body = ::File.read(::File.join(adptr.datadir, path))
+          res.body = ::File.read(target_filename)
         end
       end
     end
