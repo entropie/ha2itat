05f4836918efd9e16bed40f54b12e1a89c80a7d7
Michael Trommer <mictro@gmail.com>
2023-10-26 20:43:28 +0200

    clean

--- plugins/user/slice/actions/edit.rb
+++ plugins/user/slice/actions/edit.rb
@@ -12,24 +12,29 @@ module Ha2itat::Slices
           optional(:password1).value(:string)
         end
 
+        def enforce_permissions_for(user, req)
+          if user != session_user(req).id and not session_user(req).admin?
+            halt 503
+          end
+        end
+
         def handle(req, res)
           raise "no id" unless req.params[:user_id]
           user = adapter(:user).by_id(req.params[:user_id])
 
 
-          # p user.groups.class
-          # exit
-
-
           if req.post? and req.params.valid?
+            enforce_permissions_for(user, req)
+
             new_user_params = req.params.to_hash
 
-            # new_name = new_user_params.delete(:name)
-            # if user.name != new_name
-            #   raise "cannot change name; delete and recreate instead"
-            # end
+            new_name = new_user_params[:name]
+            if user.name != new_name
+              raise "cannot change name; delete and recreate instead"
+            end
 
-            user.populate( req.params.to_hash )
+            user.populate(new_user_params)
+
             adapter(:user).store(user)
             res.redirect_to path(:backend_user_edit, user_id: user.id)
           end
