ed2f7ea1ed52625dada6f7c82fad233e7d7ca9bd
Michael Trommer <mictro@gmail.com>
2024-12-30 01:06:29 +0100

    /api/posts/blog/...

--- plugins/blogtools/slice/config/routes.rb
+++ plugins/blogtools/slice/config/routes.rb
@@ -1,6 +1,6 @@
 module Ha2itat::Slices::Blogtools
   class Routes < Hanami::Routes
-    get "/feed(.:format)",   to: "feed",   as: :feed
-    get "/api/posts(.:format)",        to: "api",    as:  :api
+    get "/feed(.:format)",                  to: "feed",   as: :feed
+    get "/api/blog/posts(.:format)",        to: "api",    as:  :api
   end
 end
