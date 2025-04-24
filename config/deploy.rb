2efbacd3bd386c6b6e228ca0a0539c089625982a
Michael Trommer <mictro@gmail.com>
2025-04-24 16:56:19 +0200

    nixos

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,31 +26,6 @@ set :deploy_to, "/home/ha2itats/#{fetch(:application)}/"
 # Default value for :pty is false
 set :pty, true
 
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
-
-# set :ssh_options, {
-#   forward_agent: true,
-#   # auth_methods: %w(publickey),
-#   # user: "deploy",
-#   keys: %w(~/.ssh/for_remote)
-# }
-
 server "wecoso", roles: %w{web app db}
 
 set    :branch, 'master'
@@ -58,9 +33,6 @@ set    :branch, 'master'
 set    :ha2itat, release_path
 
 set    :nginx_config, "/etc/nginx/sites-enabled/ha2-#{fetch(:application)}.conf"
-#set    :init_file,    "/etc/init.d/h2-unicorn-#{fetch(:application)}"
-
-# set    :rvm_ruby_version, '3.2.2'
 
 def remote_file_exists?(full_path)
   'true' ==  capture("if [ -e #{full_path} ]; then echo 'true'; fi").strip
@@ -70,6 +42,9 @@ def remote_link_exists?(full_path)
   'true' ==  capture("if test -L #{full_path}; then echo 'true'; fi").strip
 end
 
+def nix_shell(cmd)
+  "nix-shell #{fetch(:release_path)}/shell.nix --run '#{cmd}'"
+end
 
 Rake::Task["bundler:install"].clear
 
@@ -77,10 +52,10 @@ namespace :bundler do
   task install: :config do
     on fetch(:bundle_servers) do
       within release_path do
-        with fetch(:bundle_env_variables) do
-          if fetch(:bundle_check_before_install) && test(:bundle, :check)
-            info "The Gemfile's dependencies are satisfied, skipping installation"
-          else
+        #with fetch(:bundle_env_variables) do
+          # if fetch(:bundle_check_before_install) && test(:bundle, :check)
+          #   info "The Gemfile's dependencies are satisfied, skipping installation"
+          # else
             options = []
             if fetch(:bundle_binstubs) &&
                fetch(:bundle_binstubs_command) == :install
@@ -88,8 +63,6 @@ namespace :bundler do
             end
             options << "--jobs #{fetch(:bundle_jobs)}" if fetch(:bundle_jobs)
             options << "#{fetch(:bundle_flags)}" if fetch(:bundle_flags)
-            #execute :bundle, :install, *options
-            #execute "nix-shell -p pkg-config libyaml --command 'bundle install'", *options
             ##execute "nix develop --command bundle install", *options
             #execute :nix, "develop", "--command", "bundle install", *options
             args = "'bundle install #{options.join(' ')}'"
@@ -99,8 +72,8 @@ namespace :bundler do
                fetch(:bundle_binstubs_command) == :binstubs
               execute :bundle, :binstubs, '--all', '--path', fetch(:bundle_binstubs)
             end
-          end
-        end
+          # end
+        #end
       end
     end
   end
@@ -112,15 +85,16 @@ namespace :ha2itat do
 
   task :copy_nix_shell do
     on roles(:app) do
-      sudo :cp, "/etc/nixos/res/ha2itat-shell.nix #{ release_path.join("shell.nix") }"
+      shellnix = release_path.join("shell.nix")
+      # if project has own /shell.nix dont overwrite
+      unless remote_file_exists?(shellnix)
+        sudo :cp, "/etc/nixos/res/ha2itat-shell.nix #{shellnix}"
+      end
     end
   end
 
   task :restart do
     on roles(:app) do
-      # execute fetch(:init_file), "stop"
-      # execute fetch(:init_file), "start"
-      # execute fetch(:init_file), "start"
       sudo :systemctl, "restart h2-#{fetch(:application)}"
     end
   end
@@ -140,7 +114,6 @@ namespace :ha2itat do
     end
   end
 
-
   task :link_media do
     on roles(:app) do
       ha2itat_media_path = release_path.join("media")
@@ -155,11 +128,10 @@ namespace :ha2itat do
 
   task :npm_install do
     on roles(:app) do
-      within release_path do
-        execute :bundle, "exec ruby vendor/gems/ha2itat/bin/generate_js.rb"
-        execute :npm, "install --legacy-peer-deps" # &> /dev/null"
-        execute :npm, "run production"
-      end
+      nsp = "cd #{fetch(:release_path)} && nix-shell --run '%s'"
+      execute nsp % "bundle exec ruby vendor/gems/ha2itat/bin/generate_js.rb"
+      execute nsp % "npm install --legacy-peer-deps --silent"
+      execute nsp % "npm run production --silent"
     end
   end
 
