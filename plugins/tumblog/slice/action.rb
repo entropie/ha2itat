8e5448948cb771d07bbf45ee4a7e4867760fa039
Michael Trommer <mictro@gmail.com>
2025-05-30 01:30:04 +0200

    oops

--- plugins/tumblog/slice/action.rb
+++ plugins/tumblog/slice/action.rb
@@ -1,10 +1,6 @@
 module Ha2itat::Slices::Tumblog
-  class Action < Action
+  class Action < Hanami::Action
     instance_eval(&Ha2itat::CD(:action))
-    before :check_token
-
-    include Helper::Translation::Actions
-    before :locales_setup
 
     def by_id(req)
       adapter.with_user(session_user(req)).by_id(req.params[:id])
