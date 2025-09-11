99cf553c7b4d9498042548dda7d4cbcac517ea89
Michael Trommer <mictro@gmail.com>
2025-09-11 02:51:02 +0200

    api pw

--- plugins/tumblogtools/slice/actions/api.rb
+++ plugins/tumblogtools/slice/actions/api.rb
@@ -7,6 +7,7 @@ module Ha2itat::Slices
           ret = {  }
           ret[:content] = post.to_html.strip
           ret[:date] = post.created_at.strftime("%Y-%m-%d %H%m")
+          ret[:tags] = post.tags
           ret
         end
 
@@ -16,8 +17,13 @@ module Ha2itat::Slices
           rformat = req.params.raw[:format] || "json"
           raise "nope" if rformat != "json"
 
+          if cfg_api_pw = Ha2itat.C(:api_password)
+            if cfg_api_pw != req.params[:password]
+              halt 404
+            end
+          end
           posts = tumblog_posts(req, tags: tags).map{ |pst| post_to_json(pst) }
-
+          
           res.body = posts.to_yaml
         end
       end
