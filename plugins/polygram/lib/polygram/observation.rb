666c560306e3bb30f1de778bb97458d7b4772c72
Michael Trommer <mictro@gmail.com>
2026-02-26 20:50:23 +0100

    observations/reading basics

---
+++ plugins/polygram/lib/polygram/observation.rb
@@ -0,0 +1,45 @@
+module Plugins
+  module Polygram
+
+    class Case
+      class Document
+        attr_reader :caze, :mid, :user
+        def initialize(caze, mid, user)
+          @caze = caze
+          @mid = mid
+          @user = user
+        end
+
+        def media
+          @caze.media[@mid]
+        end
+
+        def text
+          @text = ::File.read(path)
+        end
+      end
+
+      class Observation < Document
+        def self.find_or_create(caze, mid, user)
+          obs = Ha2itat.adapter(:polygram).observation_for(caze, mid, user)
+        end
+
+        def path
+          caze.path("observation/#{user.id}/#{mid}/observation.markdown")
+        end
+      end
+
+      class Reading < Document
+        def self.find_or_create(caze, mid, user)
+          obs = Ha2itat.adapter(:polygram).reading_for(caze, mid, user)
+        end
+
+        def path
+          caze.path("observation/#{user.id}/#{mid}/reading.markdown")
+        end
+      end
+    end
+
+  end
+end
+
