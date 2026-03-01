b10f41946efcf512ad48c8a27a1f37757bc500a4
Michael Trommer <mictro@gmail.com>
2026-03-01 20:47:01 +0100

    param set

--- plugins/polygram/slice/actions/show.rb
+++ plugins/polygram/slice/actions/show.rb
@@ -3,8 +3,15 @@ module Ha2itat::Slices
     module Actions
       class Show < Action
 
+        params do
+          optional(:id).filled(:string)
+        end
+
         def handle(req, res)
+          halt 422 unless req.params.valid?
+
           caze = adapter.by_id(req.params[:id])
+          halt 500 unless caze
           res.render(view, caze: caze, complete: false, menu: true)
         end
       end
